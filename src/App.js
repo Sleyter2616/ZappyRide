85adcf9814a14f23eb7d5bfee2f9d5e92ef468e6
Sleyter De La Cruz <Sleyter2616@gmail.com>
2021-03-30 20:54:43 -0400

    created header and footer, displayed across app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import {BrowserRouter, Route} from 'react-router-dom'
+import React from 'react'
+import {Container} from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 const App = () => {
-	return <div>APP</div>
+	return (
+		<BrowserRouter>
+			<Header />
+			<main className='py 3'>
+				<Container>MAIN</Container>
+			</main>
+			<Footer />
+		</BrowserRouter>
+	)
 }
 
 export default App
