b1e6fb577c6533ef3878cce1ce5dd77953d82665
Sleyter De La Cruz <Sleyter2616@gmail.com>
2021-04-04 11:14:37 -0400

    deleted excel data page not needed any longer

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import InputScreen from './Screens/InputScreen'
 import Home from './components/Home'
-import ExcelData from './Data/ExcelData'
 
 const App = () => {
 	return (
@@ -16,7 +15,6 @@ const App = () => {
 				<Container>
 					<Route path='/' exact component={Home} />
 					<Route path='/input' component={InputScreen} />
-					<Route path='/excel' component={ExcelData} />
 				</Container>
 			</main>
 			<Footer />
